c016a4607979cdd8618e21a6ab1df4d7628af8c7
moumithak <121538164+moumithak@users.noreply.github.com>
2024-10-17 14:50:36 +0530

    Update CameraScreen.js ip address

--- Frontend/app/screens/CameraScreen.js
+++ Frontend/app/screens/CameraScreen.js
@@ -4,7 +4,7 @@ import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Location from 'expo-location';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
-const BASE_URL = 'http://^^:5000'; // use your laptop's ip address when running in place of ^^
+const BASE_URL = 'http://^^:5000'; // use your network's ip address when running in place of ^^
 
 function CameraScreen(props) {
   const [hasPermission, setHasPermission] = useState(null);
